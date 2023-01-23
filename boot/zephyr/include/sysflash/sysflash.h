95b335efce730a46eac110be908c2aa0131ea98e
Emil-Juhl <emdj@bang-olufsen.dk>
2023-01-23 09:24:41 +0100

    Revert "boot: zephyr: sysflash: restore FLASH_AREA_IMAGE_x macros"
    This reverts commit 0771af0ec35a276be3d8a47d473c90fd48155a3c.
    
    Apparently this is not quite correct as it breaks flagging images as
    "active" and hence is a big no-no for our use case.
    Why this happens is still unclear to me, but this commit is the sinner.

--- boot/zephyr/include/sysflash/sysflash.h
+++ boot/zephyr/include/sysflash/sysflash.h
@@ -36,7 +36,7 @@ extern uint32_t _image_1_primary_slot_id[];
         ((x == 0) ?                            \
            PM_MCUBOOT_PRIMARY_ID :             \
          (x == 1) ?                            \
-          PM_MCUBOOT_PRIMARY_ID : \
+          PM_S0_ID : \
            255 )
 #endif
 
@@ -44,7 +44,7 @@ extern uint32_t _image_1_primary_slot_id[];
         ((x == 0) ?                   \
             PM_MCUBOOT_SECONDARY_ID:  \
         (x == 1) ?                    \
-           PM_MCUBOOT_SECONDARY_ID :  \
+           PM_S1_ID :  \
            255 )
 #else
 
